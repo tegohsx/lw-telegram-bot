ac64ca71d7ca409edeaf94ad857d6dc58559a922
Teguh Santoso <101353193+tegohsx@users.noreply.github.com>
2023-02-24 15:52:30 +0700

    Update index.js

--- sample_complex/index.js
+++ sample_complex/index.js
@@ -2,13 +2,13 @@ const { bot } = require('../index')
 const command = require('./commad')
 
 //calculate cube volume and surface area
+bot.start(command.start)
+bot.command('cubevolume', command.cubevolume)
+bot.command('cubesurfacearea', command.cubesurfacearea)
+
 bot.init({
     secret_path: '',
     port: 2220, 
     token: '5696900399:AAFbNqgZ7AGF6OFGAz0VJV2z0f36zm_vArg',
     webhook: false
-},()=>{
-    bot.start(command.start)
-    bot.command('cubevolume', command.cubevolume)
-    bot.command('cubesurfacearea', command.cubesurfacearea)
 })
