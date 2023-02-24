0737b48b9619b8785ed9afd814b8dcee9b6bf465
Teguh Santoso <101353193+tegohsx@users.noreply.github.com>
2023-02-24 15:54:21 +0700

    Update inlineKeyboard.js

--- sample/inlineKeyboard.js
+++ sample/inlineKeyboard.js
@@ -1,43 +1,42 @@
 const { bot, Telegram } = require('../index')
 
+bot.start((ctx) => {
+    ctx.telegram.sendMessage(ctx.chat_id, 'Welcome to *MyBot*', {
+        parse_mode: 'markdown',
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {
+                        text: 'Halo',
+                        callback_data: 'tapHalo'
+                    },
+                    {
+                        text: 'Hi',
+                        callback_data: 'tapHiYou'
+                    }
+                ]
+            ]
+
+        }
+    })
+})
+
+bot.callbackQueryData('tapHalo', (ctx)=>{
+    Telegram.sendMessage(ctx.chat_id, 'You were tap Halo').catch(()=>false) //use catch to error handling
+    // Telegram.sendMessage is equal to ctx.telegram.sendMessage
+})
+
+bot.callbackQueryData(/^tapHi/, (ctx)=>{
+    ctx.telegram.sendMessage(ctx.chat_id, 'You were tap Hi').then((res)=>{
+        console.log(res) //return axios response
+    }).catch((err)=>{
+        console.log(err) //return axios error
+    })
+})
+
 bot.init({
     secret_path: '',
     port: 2220, 
     token: '5696900399:AAFbNqgZ7AGF6OFGAz0VJV2z0f36zm_vArg',
     webhook: false
-}, () => {
-    bot.start((ctx) => {
-        ctx.telegram.sendMessage(ctx.chat_id, 'Welcome to *MyBot*', {
-            parse_mode: 'markdown',
-            reply_markup: {
-                inline_keyboard: [
-                    [
-                        {
-                            text: 'Halo',
-                            callback_data: 'tapHalo'
-                        },
-                        {
-                            text: 'Hi',
-                            callback_data: 'tapHiYou'
-                        }
-                    ]
-                ]
-
-            }
-        })
-    })
-
-    bot.callbackQueryData('tapHalo', (ctx)=>{
-        Telegram.sendMessage(ctx.chat_id, 'You were tap Halo').catch(()=>false) //use catch to error handling
-        // Telegram.sendMessage is equal to ctx.telegram.sendMessage
-    })
-
-    bot.callbackQueryData(/^tapHi/, (ctx)=>{
-        ctx.telegram.sendMessage(ctx.chat_id, 'You were tap Hi').then((res)=>{
-            console.log(res) //return axios response
-        }).catch((err)=>{
-            console.log(err) //return axios error
-        })
-    })
-
 })
