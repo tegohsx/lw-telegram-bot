33be77588d2711eeb77e6e334ce93705a04f5d19
Teguh Santoso <101353193+tegohsx@users.noreply.github.com>
2023-02-24 15:53:26 +0700

    Update replyMessage.js

--- sample/replyMessage.js
+++ sample/replyMessage.js
@@ -1,27 +1,26 @@
 const { bot } = require('../index')
 
+bot.start((ctx) => {
+    ctx.telegram.sendMessage(ctx.chat_id, 'Welcome to *MyBot*', {
+        parse_mode: 'markdown'
+    })
+})
+
+bot.text('PING!!!', (ctx) => {
+    ctx.replyWithText('PONG!!!').catch(err=>{
+        console.log(err)
+    })
+})
+
+bot.command('ping', (ctx) => {
+    ctx.replyWithText('PONG!!!').catch(err=>{
+        console.log(err)
+    })
+})
+
 bot.init({
     secret_path: '',
     port: 2220, 
     token: '5696900399:AAFbNqgZ7AGF6OFGAz0VJV2z0f36zm_vArg',
     webhook: false
-}, () => {
-    bot.start((ctx) => {
-        ctx.telegram.sendMessage(ctx.chat_id, 'Welcome to *MyBot*', {
-            parse_mode: 'markdown'
-        })
-    })
-
-    bot.text('PING!!!', (ctx) => {
-        ctx.replyWithText('PONG!!!').catch(err=>{
-            console.log(err)
-        })
-    })
-
-    bot.command('ping', (ctx) => {
-        ctx.replyWithText('PONG!!!').catch(err=>{
-            console.log(err)
-        })
-    })
-
 })
